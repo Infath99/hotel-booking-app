8b13624e6c0c424fc0a6fb6c498056fc6b724d58
mohamedInfath <mohamedinfath99@gmail.com>
2022-12-22 21:47:04 +0530

    Email Component Done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import Header from '../../components/Header/Header'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import MailSection from '../../components/mailComponent/MailSection'
 
 function home() {
   return (
@@ -15,10 +16,13 @@ function home() {
 
       <div className="homeContainer">
         <Featured />
+        
         <h1 className='homeTitle'>Browse by property type</h1>
         <PropertyList />
+
         <h1 className='homeTitle'>Homes guests love</h1>
         <FeaturedProperties />
+        <MailSection />
       </div>
     </div>
   )
